1daa2fc38d0df7d6c3fd0035391020b6a82ccc7c
arfin-bs23 <hafizur.rahman@brainstation23.com>
2020-04-27 23:29:06 +0600

    reshuffled

--- src/main/java/com/moviedatabase/gatewayserver/GatewayConfiguration.java
+++ src/main/java/com/moviedatabase/gatewayserver/GatewayConfiguration.java
@@ -5,12 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class GatewayConfiguration {
-    @Bean
-    public RouteLocator getRoutes(RouteLocatorBuilder builder){
-        return builder.routes().route(
-                r-> r.path("/movie/**").uri("lb://movie-service").id("movieService")
-        ).build();
-    }
+//    @Bean
+//    public RouteLocator getRoutes(RouteLocatorBuilder builder){
+//        return builder.routes().route(
+//                r-> r.path("/movie/**").uri("lb://movie-service").id("movieService")
+//        ).build();
+//    }
 }
